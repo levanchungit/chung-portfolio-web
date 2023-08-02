dd9b1b0e8be0d07a4f50ca2477ed9c342a09d66d
tvschung <chung.le@tinvietsoft.com>
2023-08-02 22:29:25 +0700

    reponsive

--- components/ItemWork.tsx
+++ components/ItemWork.tsx
@@ -1,40 +1,43 @@
 import Image from "next/image";
 
 type Props = {
+  index: number;
   id: number;
   title: string;
   image: string;
 };
 
-export default function ItemWork({ id, title, image }: Props) {
+export default function ItemWork({ index, id, title, image }: Props) {
   return (
-    <div className="flex flex-row justify-end items-center relative">
-      <div className="flex flex-col text-white w-[200px] absolute left-0">
-        <h2 className="font-bold">{title}</h2>
-        <div className="flex flex-col h-px w-full bg-main"></div>
-        <h2 className="">{id > 9 ? id : `0${id}`}</h2>
-        <svg
-          width="12"
-          height="14"
-          viewBox="0 0 12 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.49519 0.504761L11.7452 6.99995L0.49519 13.4951L0.49519 0.504761Z"
-            fill="#D9D9D9"
-          />
-        </svg>
-      </div>
+    <div className={`sm: flex `}>
+      <div className="flex flex-row  items-center">
+        <div className="flex flex-col text-white w-[200px] ">
+          <h2 className="font-bold sm: text-xl">{title}</h2>
+          <div className="flex flex-col h-px w-full bg-main"></div>
+          <h2 className="">{id > 9 ? id : `0${id}`}</h2>
+          <svg
+            width="12"
+            height="14"
+            viewBox="0 0 12 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.49519 0.504761L11.7452 6.99995L0.49519 13.4951L0.49519 0.504761Z"
+              fill="#D9D9D9"
+            />
+          </svg>
+        </div>
 
-      <div className="flex">
-        <Image
-          alt="Picture of the author"
-          src={image}
-          width={200}
-          height={100}
-          className="cursor-pointer"
-        />
+        <div className="">
+          <Image
+            alt="Picture of the author"
+            src={image}
+            width={200}
+            height={100}
+            className="cursor-pointer sm:min-w-[400px]"
+          />
+        </div>
       </div>
     </div>
   );
--- containers/work.tsx
+++ containers/work.tsx
@@ -3,26 +3,29 @@ import { itemWorks } from "@/global/constants";
 
 export default function Work() {
   return (
-    <div className="work-container">
-      <div className="w-0.5 bg-main rounded-full relative sm:left-[20%]">
+    <div className="flex w-full bg-background-start bg-cover overflow-hidden">
+      <div className="w-0.5 bg-main rounded-full relative top-[45%] left-10 sm:left-[20%]">
         <div className="flex flex-col w-2 h-2 rounded-full absolute top-0 left-1/2 transform -translate-x-1/2 bg-main"></div>
       </div>
-      <div className="flex flex-col gap-5 pb-8">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-moon_mist sm:text-xl">Work/&gt;</h2>
-          <p className="text-white">
-            Selected UI web, mobile, video projects...
-          </p>
-        </div>
-        <div className="flex flex-col gap-8">
-          {itemWorks.map((item) => (
-            <ItemWork
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              image={item.image}
-            />
-          ))}
+      <div className="work-container">
+        <div className="flex flex-col pb-8 sm:w-full ">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-moon_mist sm:text-xl">Work/&gt;</h2>
+            <p className="text-white">
+              Selected UI web, mobile, video projects...
+            </p>
+          </div>
+          <div className="flex flex-col gap-8">
+            {itemWorks.map((item, index) => (
+              <ItemWork
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                index={index}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
